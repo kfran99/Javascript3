ba41807ec57cc5edf8a2b3a6ee0ec98dc3de9e66
Hackbright Student <user@mini-004.(none)>
2014-03-04 17:46:07 -0800

    Lists, items, and buttons done. Need to not reload page for back button.

--- api.py
+++ api.py
@@ -15,29 +15,45 @@ def index():
 
 @app.route('/', methods=['POST'])
 def index_post():
+    #create a new list
     list_name = request.form.get('todo_list_name')
-    if not list_name:
-        flash("I'm sorry, but you'll need to specify a list name")
+    todo_lists.insert(dict(list_name=list_name))
 
-    else:
-        todo_lists.insert(dict(list_name=list_name))
-
-        flash('Todo list %s created' % list_name)
+    # return all of the lists (including our new one)
+    results = todo_lists.all()
+    lists = [l for l in results]
+    return render_template("todo_list_partial.html", lists=lists)
+    
 
     return redirect('/')
+
+#@app.route('/ajax/main_list', methods=['GET'])
+# def ajax_todo():
+#     list_name = request.form.get('todo_list_name')
+#     if not list_name:
+#         return "Invalid List Name"
+
+#     todo_lists.insert(dict(list_name=list_name))
+
+#     return list_name
+
 
 @app.route('/todo_lists/<int:id>')
 def todo_list_show(id):
     items = todo_item_table.find(todo_list_id=id)
     items = [x for x in items]
 
-    return render_template("todo_list.html", items=items)
+    return render_template("todo_list.html", items=items, id=id)
 
 @app.route('/todo_lists/<int:id>', methods=["POST"])
 def todo_item_create(id):
-    resp = todo_item_table.insert(dict(task=request.form.get("task"), todo_list_id=id, done=False))    
-
-    return redirect("/todo_lists/%d" % id)
+    #Create a new todo_item
+    todo_item_table.insert(dict(task=request.form.get("task"), todo_list_id=id, done=False))
+    
+    #return all of the items for given list
+    items = todo_item_table.find(todo_list_id=id)
+    items = [x for x in items] 
+    return render_template("todo_items_partial.html", items=items)
 
 
 @app.route('/todo_lists/poll')
--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,48 @@
 $(document).ready(function(){
+
+    $("#form_submit").on("click", function(event){
+        event.preventDefault();
+        $.ajax({
+            url: "/",
+            method: "POST",
+            data: $("form#todo_list_form").serialize()
+        }).done(function(data){
+            //alert(data);
+            $("#actual-list").html(data);
+        }).fail(function(){
+            alert('fail!!!');
+        });
+    });
+    $("#form_submit2").on("click", function(event){
+        event.preventDefault();
+        $.ajax({
+            url: "/todo_lists/" + $("#todoId").val(),
+            method: "POST",
+            data: $("form#todo-item-form").serialize()
+        }).done(function(data){
+            //alert(data);
+            $("#actual-items").html(data);
+        }).fail(function(){
+            alert('fail!!!');
+        });
+    });
+    $("#link_button").on("click", function(event){
+        event.preventDefault();
+        location.replace('/');
+    });
+
 });
+
+var getListsFromServer = function() {
+    $.ajax({
+        url:'/todo_lists/poll',
+        method: "GET"
+    }).done(function(data){
+        $("#actual-list").html(data);
+    });
+};
+
+
+
+
+
